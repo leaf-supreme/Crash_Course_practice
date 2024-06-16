cab09772261d560cfbbf9b043e19eab58ed77d70
leaf-supreme <kylem.hendrickson@gmail.com>
2024-06-15 22:36:14 -0700

    attempt to publish

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Crash_Course_practice
-This is my practice space to learn python
+# This is my practice space to learn python
+# This is new code
+
---
+++ weather_bot.py
@@ -0,0 +1,9 @@
+print("this may show up in repository")
+
+weather = input("is it rain or sunny: ") 
+rain = False
+if rain:
+    print("bring a jacket")
+else:
+    print("put on them hoochie daddy shorts")
+# should be new code on Git
