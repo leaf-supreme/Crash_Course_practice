47e80eba7bd87cf228887a25dcd9e6e09fc1bc3e
leaf-supreme <kylem.hendrickson@gmail.com>
2024-06-15 23:58:59 -0700

    finish chapter 3

--- Crash_Course_prac.py
+++ Crash_Course_prac.py
@@ -16,3 +16,18 @@ print(f"Hello, {full_name.title()}!")
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
 #first_name.rstrip(), first_name.lstrip(), first_name.strip(), 
 # first_name.removesuffix(), first_name.removeprefix()
+
+# simple data set 
+bicycles = ['trek', 'cannondale', 'santa cruz', 'fuji', 'look', 'pinarello', 'specalized', 'surley', 'all-city', 'giant']
+first_bike = (f"My First Bicycle was a {bicycles[6]}")
+print(first_bike)
+bicycles[0] = 'trekk'
+bicycles.append('canyon') # always adds it to the end
+bicycles.insert(2, 'fairlight')
+last_item = bicycles.pop() #removes last item on list but lets you use it first, you can also specify .pop(2)...
+print(f"The last bike i will not own is a {last_item}")
+to_expensive = bicycles.remove('pinarello') # doesnt remove all instances of item, use loop to remove
+sorted_bikes = bicycles.sort()  # bicycles.sort(reverse=TRUE), displays sorted but not permanent, bicycles.reverse() is permanent
+
+
+
