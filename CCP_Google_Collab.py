8bc00749eaf3bd06ac4c8a032887ec692e65ed47
leaf-supreme <kylem.hendrickson@gmail.com>
2024-06-15 23:01:45 -0700

    Adding code from Google Collab

---
+++ CCP_Google_Collab.py
@@ -0,0 +1,18 @@
+#CCP_Google_Collab
+
+print("This is where the world ends")
+print("If i say hello world one more time, I may just take it over")
+#import this # this is a poem by tim peters for code edequite, kind of fun
+name = "hello to all!"
+name.title() # upper and lower as well
+
+#insert a variable into a string, f variable
+first_name = " 'bob' "
+last_name = " dole "
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+#white spaces you can add whitespace or strip it
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+#first_name.rstrip(), first_name.lstrip(), first_name.strip(), 
+# first_name.removesuffix(), first_name.removeprefix()
