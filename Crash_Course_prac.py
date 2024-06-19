c5dd58b119746509365770361394c79c142f6af8
leaf-supreme <kylem.hendrickson@gmail.com>
2024-06-19 00:00:32 -0700

    chapter excercises 4.1-4.9

--- Crash_Course_prac.py
+++ Crash_Course_prac.py
@@ -1,4 +1,4 @@
-#CCP_Google_Collab
+#C
 
 print("This is where the world ends")
 print("If i say hello world one more time, I may just take it over")
@@ -22,12 +22,76 @@ bicycles = ['trek', 'cannondale', 'santa cruz', 'fuji', 'look', 'pinarello', 'sp
 first_bike = (f"My First Bicycle was a {bicycles[6]}")
 print(first_bike)
 bicycles[0] = 'trekk'
-bicycles.append('canyon') # always adds it to the end
+bicycles.append('canyon')# always adds it to the end
+bicycles.append('huffy')
 bicycles.insert(2, 'fairlight')
 last_item = bicycles.pop() #removes last item on list but lets you use it first, you can also specify .pop(2)...
 print(f"The last bike i will not own is a {last_item}")
 to_expensive = bicycles.remove('pinarello') # doesnt remove all instances of item, use loop to remove
 sorted_bikes = bicycles.sort()  # bicycles.sort(reverse=TRUE), displays sorted but not permanent, bicycles.reverse() is permanent
 
+# For loops practice 
+print("Lets look at all the bike brands")
+for bikes in bicycles:  # remember bikes is the variable for the bicycles list in this loop
+    print(f"{bikes.title()} makes a good bike")
+    print(f"{bikes.title()} would make a great compnaion")
+print("That was a long list of bikes")
 
+# range function is useful for numbers
+numbers = range(1,5) # range(6) is 0-5, 6 not displayed
+even_list = list(range(2,11,2))
 
+squares = []
+for value in range(1,6):
+    square = value**2
+    squares.append(square) #both lines can be replaced by square.append(value**2)
+print(squares)
+
+# List comprehensions (somewhat advanced), can do the same as the 4 lines above (no colon used)
+# combines a for loop and creation of new elements to fill an empty list that appends it each time
+squarez = [value**2 for value in range(1,11)] 
+print(squarez)
+
+# hopefully a place to do some of the Crash Course Python Practice modules
+# 4.3
+numbers = list(range(0, 21)) # i had to look this up
+for value in numbers:
+    print(value)
+
+#4.4 on million
+million = list(range(0,1000001))
+#for value in million:
+    #print(value)   # crazy long use Ctrl+C in terminal to close
+
+#4.5 summin a million
+print(min(million))
+print(max(million))
+print(sum(million))  # did that in the blink of an eye 8)
+
+#4.6 odd numbers
+noomber = list(range(1,20,2))
+for value in noomber:
+    print(value)
+
+#4.7 Threes
+normber = list(range(0,30,3))
+for value in normber:
+    print(value)
+
+#4.8 cubes
+number = list(range(0,11))
+for value in number:
+    print(value**3)
+#The way they did it with an empty list VVV
+cubes = []
+for value in range(1,11):
+    cube = value**3
+    cubes.append(cube)
+for cube in cubes:
+    print(cube)
+
+#4.9 cube comprehension
+cube = [x**3 for x in range(1,11)]  #found similar online but got it first try
+print(cube)
+for cube in cubes:
+    print(cube)
