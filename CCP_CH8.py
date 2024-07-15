dff96600a677581c182a6fe23024791be5b02502
leaf-supreme <kylem.hendrickson@gmail.com>
2024-07-14 23:40:51 -0700

    init CCP CH8

---
+++ CCP_CH8.py
@@ -0,0 +1,8 @@
+#_______________________________________________________________________________
+#_______________________Chapter 8 - Functions Practice__________________________
+# 8.1 Message 
+def display_massage():
+    """Displayes a what CCP_CH8 is about"""
+    print('This Chapter is about defining functions')
+    
+display_massage()
